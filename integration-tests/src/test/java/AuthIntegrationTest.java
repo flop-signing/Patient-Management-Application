cf535be3d7a521e098ed92145630b8f59e7cac88
flop-signing <mehedy.mmc272@gmail.com>
2025-09-04 08:56:00 +0600

    Login and Unauth Integration Test

--- integration-tests/src/test/java/AuthIntegrationTest.java
+++ integration-tests/src/test/java/AuthIntegrationTest.java
@@ -1,2 +1,62 @@
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.Matchers.notNullValue;
+
 public class AuthIntegrationTest {
+    @BeforeAll
+    static void setup() {
+        RestAssured.baseURI = "http://localhost:4004/";
+    }
+
+    // Three steps to create test
+    // 1. Arrange
+    // 2. Act
+    // 3. Assert
+
+
+    @Test
+    public void shouldReturnOKWithValidToken() {
+        String loginPayload = """
+                {
+                    "email":"testuser@test.com",
+                    "password":"password123"
+                }
+                """;
+
+        Response response = RestAssured.given()
+                .contentType("application/json")
+                .body(loginPayload)
+                .when()
+                .post("/auth/login")
+                .then()
+                .statusCode(200)
+                .body("token", notNullValue())
+                .extract()
+                .response();
+
+        System.out.println("Generated token: " + response.jsonPath().getString("token"));
+    }
+
+    @Test
+    public void shouldReturnUnauthorizedOnInvalidLogin() {
+        String loginPayload = """
+                {
+                    "email":"invalid_test@test.com",
+                    "password":"wrongPassword"
+                }
+                """;
+
+        RestAssured.given()
+                .contentType("application/json")
+                .body(loginPayload)
+                .when()
+                .post("/auth/login")
+                .then()
+                .statusCode(401);
+
+    }
+
 }
