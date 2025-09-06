2ea4d2b53b0346b2cd2a40670101bc781834ee06
flop-signing <mehedy.mmc272@gmail.com>
2025-09-07 00:28:38 +0600

    create vpc

--- infrastructure/src/main/java/com/pm/stack/LocalStack.java
+++ infrastructure/src/main/java/com/pm/stack/LocalStack.java
@@ -2,22 +2,33 @@ package com.pm.stack;
 
 
 import software.amazon.awscdk.*;
+import software.amazon.awscdk.services.ec2.Vpc;
 
 public class LocalStack extends Stack {
 
-    public LocalStack(final App scope, final String id, final StackProps props){
-        super(scope, id,props);
+    private final Vpc vpc;
+
+
+    public LocalStack(final App scope, final String id, final StackProps props) {
+        super(scope, id, props);
+        this.vpc = createVpc();
     }
 
+
+    private Vpc createVpc() {
+        return Vpc.Builder.create(this, "PatientManagementVPC").vpcName("PatientManagementVPC").maxAzs(2).build();
+    }
+
+
     public static void main(String[] args) {
-        App app=new App(AppProps.builder().outdir("./cdk.out").build());
+        App app = new App(AppProps.builder().outdir("./cdk.out").build());
 
         // synthesizer is converted to our java code into cloud formation template.
-        StackProps props=StackProps.builder()
+        StackProps props = StackProps.builder()
                 .synthesizer(new BootstraplessSynthesizer())
                 .build();
 
-        new LocalStack(app,"localstack",props);
+        new LocalStack(app, "localstack", props);
         app.synth();
 
         System.out.println("App Synthesizing in progress...");
